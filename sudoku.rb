839057acda474c01222875cdffe821f3bfc6aaef
Ryan Murphy <ryanmurphy222@gmail.com>
2017-06-01 14:38:18 -0700

    made sure all ends present, commented unnecessary lines

--- sudoku.rb
+++ sudoku.rb
@@ -31,7 +31,7 @@ class SudokuGame
       begin
         pos = parse_pos(gets)
       rescue
-        TODO: Google how to print the error that happened inside of a rescue statement.
+        #TODO: Google how to print the error that happened inside of a rescue statement.
         puts "Invalid position entered (did you use a comma?)"
         puts ""
 
@@ -75,6 +75,7 @@ class SudokuGame
       return true
     else
       get_pos
+    end
   end
 
   def valid_val?(val)
